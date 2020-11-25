89094c2be15e34117bd134d3c953418d5440f5b1
KarthikUthira <60420407+KarthikUthira@users.noreply.github.com>
2020-11-25 13:54:18 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,5 @@
+text="i will win definitely"
+x=text.casefold()
+str="karthik uthira"
+print(str[2:])
+print(x)
---
+++ task2.py
@@ -0,0 +1,24 @@
+#assigning same values
+
+a=b=c=25
+a=a/10
+b=b*50
+c=c+60
+print(a)
+print(b)
+print(c)
+
+#replacing string 
+
+str="world"
+str.replace('r','G')
+print(str)
+
+#explicit conversion
+
+c="30"
+d=3.0
+c=float(c)
+d=int(d)
+print(c)
+print(d)
