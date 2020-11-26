beac5696c0918503a3b601b3a584848b2ef54b45
KarthikUthira <60420407+KarthikUthira@users.noreply.github.com>
2020-11-26 20:31:46 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,28 @@
+#adding an item
+
+lis=[1,2,3,4,5]
+lis.append(64)
+print(lis)
+
+#deleting
+
+del lis
+
+#largest number from list
+
+li=[1,2,3,4,5]
+print(max(li))
+
+#smallest number
+
+print(min(li))
+
+#tuple-reversing
+
+tup=(12,23,76,45,12)
+print(tup[::-1])
+
+#tup-list conversion
+
+tup=list(tup)
+print(tup)
